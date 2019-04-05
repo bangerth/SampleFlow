dd00b27c22fc8efcf4cd2d3ae8b6c78e77dfd230
Wolfgang Bangerth <bangerth@colostate.edu>
2019-04-05 16:30:07 -0600

    Document most of the Consumer classes.

--- include/sampleflow/consumer.h
+++ include/sampleflow/consumer.h
@@ -24,23 +24,113 @@
 
 namespace SampleFlow
 {
+  /**
+   * This is the base class for classes that *consume* samples, i.e. react
+   * in some way to a sample produced by a Producer object. Examples of
+   * consumers are classes that compute the average $\left<x\right>$ over
+   * the samples $x_k$ they are sent (implemented in Consumers::MeanValue),
+   * the standard deviation or covariance matrix (implemented in
+   * Consumers::StandardDeviation and Consumers::Covariance), or that
+   * simply output each sample to a `std::ostream` (implemented in
+   * Consumers::StreamOutput). A special type of consumers are classes
+   * derived from the Filter class: These generally don't carry any significant
+   * state around (such as the mean value of previously received samples) but
+   * instead transform each sample $x_k$ into a different $y_k=f(x_k)$.
+   * The Filter class has more examples on this.
+   *
+   * The current class has a rather minimalist interface: Its
+   * connect_to_producer() member function is used to attach a Consumer
+   * object to a Producer object; whenever the producer then generates
+   * a sample, it indicates the value of the sample to all connected
+   * consumers through a signal that in connect_to_producer() is set to
+   * call the consumer() member function of this class. The consume()
+   * member function is `virtual` and abstract and needs to be implemented
+   * in derived classes to do whatever that derive class wants to do with
+   * each sample.
+   *
+   *
+   * ### Threading model ###
+   *
+   * Consumers can be attached to multiple producers (see the example in
+   * the documentation of the connect_to_producer() function), and these
+   * producers may be running on separate threads. As a consequence,
+   * implementations of classes derived from the Consumer base class need
+   * to expect that their member functions can be called from different
+   * threads, and, more importantly, concurrently. As a consequence, it
+   * is important that all member functions of derived classes use
+   * appropriate strategies for dealing with concurrency. Principally,
+   * this implies that all functions that access the current state of
+   * their object need to use `std::mutex` and `std::lock_guard` objects
+   * appropriately.
+   *
+   *
+   * @tparam InputType The C++ type used to describe samples. For example,
+   *   if one samples from a continuous, one-dimensional distribution, then
+   *   an appropriate type may be `double`. If one samples from the two
+   *   sides of a coin, then `bool` may be the appropriate choice.
+   */
   template <typename InputType>
   class Consumer
   {
     public:
+      /*
+       * The destructor. It disconnects this consumer object from
+       * all producers it was connected to.
+       */
       virtual
       ~Consumer ();
 
+      /**
+       * A member function typically called from user code to connect
+       * this consumer object to a producer object. As a consequence,
+       * every time the producer generates a sample, it will call an internal
+       * function of the current object which in turn then calls the
+       * consumer() member function that needs to be implemented in derived
+       * classes.
+       *
+       * Note that this function can be called for multiple producers. This
+       * would connect the same consumer to multiple producers -- an
+       * application of this facility would be if a program were to run
+       * multiple sampling algorithms in parallel on separate threads. In
+       * such cases, it might still be useful to compute the mean value
+       * over all samples produced by all of the sampling objects. One
+       * would do this by connecting the same mean value consumer object
+       * to all samplers by calling this function several times with
+       * different arguments.
+       *
+       * @param[in] producer A reference to the producer object whose
+       *   samples we want to consumer in the current object.
+       */
       void
       connect_to_producer (Producer<InputType> &producer);
 
-      virtual void
+      /**
+       * The main function of this class. It is the only function a
+       * derived class needs to implement. It receives both the sample
+       * and some additional information about this sample as argument.
+       *
+       * @param[in] sample A sample $x_k$.
+       * @param[in] aux_data Additional information the producer that
+       *   generated the sample may have wanted to convey along with
+       *   the same value itself.
+       */
+      virtual
+      void
       consume (InputType sample,
                AuxiliaryData aux_data) = 0;
 
     private:
+      /**
+       * A list of connections created by calling connect_to_producer().
+       * We store this list so that we can terminate the connection once
+       * the current object is destroyed, in order to avoid triggering
+       * a slot that no longer exists if the originally connected
+       * producer decides to generate a sample after the current object
+       * has been destroyed.
+       */
       std::list<boost::signals2::connection> connections_to_producers;
   };
+
 
 
   template <typename InputType>
@@ -51,11 +141,14 @@ namespace SampleFlow
   }
 
 
+
   template <typename InputType>
   void
   Consumer<InputType>::
   connect_to_producer (Producer<InputType> &producer)
   {
+    // Create a connection to a lambda function that in turn calls
+    // the consume() member function of the current object.
     connections_to_producers.push_back (
       producer.connect_to_signal (
         [&](InputType sample, AuxiliaryData aux_data)
@@ -63,6 +156,19 @@ namespace SampleFlow
       this->consume (std::move(sample), std::move(aux_data));
     }));
   }
+
+
+  /**
+   * A namespace for the implementation of consumers, i.e., classes
+   * derived from the Consumer class.
+   *
+   * Strictly speaking, this should also include filters (i.e., classes
+   * derived from the Filter class), but since these are in addition
+   * derived from the Producer class, they are in their own namespace
+   * Filters.
+   */
+  namespace Consumers
+  {}
 }
 
 #endif
--- include/sampleflow/consumers/stream_output.h
+++ include/sampleflow/consumers/stream_output.h
@@ -25,20 +25,68 @@ namespace SampleFlow
 {
   namespace Consumers
   {
+    /**
+     * A Consumer class that outputs each sample it receives to a
+     * `std::ostream` object. This can be used to write all samples into
+     * a file, for example.
+     *
+     *
+     * ### Threading model ###
+     *
+     * The implementation of this class is thread-safe, i.e., its
+     * consume() member function can be called concurrently and from multiple
+     * threads.
+     *
+     *
+     * @tparam InputType The C++ type used for the samples $x_k$. In
+     *   order to output it through an object of type `std::ostream`,
+     *   it must be possible to write
+     *   @code
+     *      stream << sample;
+     *   @endcode
+     *   where `sample` is of type `InputType`.
+     */
     template <typename InputType>
     class StreamOutput : public Consumer<InputType>
     {
       public:
+        /**
+         * Constructor.
+         *
+         * @param[in] output_stream A reference to the stream to which output
+         *   will be written for each sample. This class stores a reference
+         *   to this stream object, so it needs to live at least as long
+         *   as the current object.
+         */
         StreamOutput (std::ostream &output_stream);
 
+        /**
+         * Process one sample by outputting it to the stream set in the
+         * constructor.
+         *
+         * @param[in] sample The sample to process.
+         * @param[in] aux_data Auxiliary data about this sample. The current
+         *   class does not know what to do with any such data and consequently
+         *   simply ignores it.
+         */
         virtual
         void
         consume (InputType sample, AuxiliaryData /*aux_data*/) override;
 
       private:
+        /**
+         * A mutex used to lock access to all member variables when running
+         * on multiple threads.
+         */
         mutable std::mutex mutex;
+
+        /**
+         * A reference to the stream to which output will be written for each
+         * sample.
+         */
         std::ostream &output_stream;
     };
+
 
 
     template <typename InputType>
