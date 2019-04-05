27f56fac3acf7d0cf123f3c29a1d6f3b75f0d522
Wolfgang Bangerth <bangerth@colostate.edu>
2019-04-05 10:28:27 -0600

    Document the producer classes.

--- include/sampleflow/producer.h
+++ include/sampleflow/producer.h
@@ -23,16 +23,69 @@
 
 namespace SampleFlow
 {
+  /**
+   * This is the base class for classes that *produce* samples. Principally,
+   * it provides a way for Consumer objects to attach themselves to a signal
+   * through which they will then be informed whenever a new sample has been
+   * produced. Consumer objects can do this by calling the connect_to_signal()
+   * function with a function object that will be called whenever a new
+   * sample has been produced. As many consumers as desired can be connected
+   * to this signal.
+   *
+   * Derived classes are free to implement whatever algorithm they want in
+   * generating these samples. (The Filter class is one example of an
+   * implementation: It is both a Producer and a Consumer, and everytime it
+   * "consumes" a sample, i.e., it called from another producer with a
+   * new sample, it decides whether it wants to convert this input into
+   * an output sample of its own.) In general, implementations of derived
+   * classes signal the availability of a new sample by triggering the
+   * `issue_sample` member variable of this class, which then passes on the
+   * sample (and any auxiliary data that may be available along with the
+   * sample) to all consumers that have connected to the sample.
+   *
+   * @tparam OutputType The C++ type used to describe samples. For example,
+   *   if one samples from a continuous, one-dimensional distribution, then
+   *   an appropriate type may be `double`. If one samples from the two
+   *   sides of a coin, then `bool` may be the appropriate choice.
+   */
   template <typename OutputType>
   class Producer
   {
     public:
+      /**
+       * Connect the function passed as argument to the signal that is
+       * triggered whenever a new sample is produced. All function
+       * objects attached by calling the current function are then called
+       * every time a new sample becomes available.
+       *
+       * @param[in] f The function to be called whenever a new sample is produced.
+       *   This may simply be a pointer to a function that takes two arguments
+       *   (one of the type of the sample, and one of type AuxiliaryData), but
+       *   more frequently it will be a lambda function that takes two
+       *   arguments, or something that has been created using the std::bind
+       *   functionalities.
+       *
+       * @return The returned object describes the connection made with
+       *   the signal to which the caller wants to attach `f`. Callers
+       *   may want to store this connection object and, if the calling
+       *   object is destroyed, terminate the connection using
+       *   `connection.disconnect()`. This ensures that whenever the signal
+       *   is triggered, the function previously attached is no longer
+       *   called.
+       */
       boost::signals2::connection
       connect_to_signal (const std::function<void (OutputType, AuxiliaryData)> &f);
 
     protected:
+      /**
+       * The signal that is used to notify downstream objects of the
+       * availability of a new sample. Implementations of derived
+       * classes should call this signal whenever a new sample has
+       * been produced.
+       */
       boost::signals2::signal<void (OutputType, AuxiliaryData)> issue_sample;
   };
+
 
 
   template <typename OutputType>
@@ -40,8 +93,22 @@ namespace SampleFlow
   Producer<OutputType>::
   connect_to_signal (const std::function<void (OutputType, AuxiliaryData)> &f)
   {
+    // Connect with the signal and return the connection object.
     return issue_sample.connect (f);
   }
+
+
+  /**
+   * A namespace for the implementation of producers, i.e., classes
+   * derived from the Producer class.
+   *
+   * Strictly speaking, this should also include filters (i.e., classes
+   * derived from the Filter class), but since these are in addition
+   * derived from the Consumer class, they are in their own namespace
+   * Filters.
+   */
+  namespace Producers
+  {}
 }
 
 
--- include/sampleflow/producers/metropolis_hastings.h
+++ include/sampleflow/producers/metropolis_hastings.h
@@ -25,14 +25,63 @@ namespace SampleFlow
 {
   namespace Producers
   {
+    /**
+     * An implementation of the Metropolis-Hastings algorithms sampling
+     * from a probability distribution $\pi(x)$ defined on objects $x$ of type
+     * `OutputType`. This class is therefore an implementation of the
+     * "Producer" idiom in SampleFlow. For a discussion of the
+     * Metropolis-Hastings method, see
+     * https://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm .
+     *
+     * The Metropolis-Hastings algorithms, often abbreviates as the
+     * "MH sampler" requires three inputs: a starting sample, a way to
+     * evaluate the value of the (non-normalized) probability density
+     * function for some value of the sample space, and a way to
+     * "perturb" a given sample to get a new sample. In the context
+     * of sampling algorithms, the non-normalized probability
+     * density $\pi(x)$ is typically called the "likelihood";
+     * for implementational stability, this class requires users
+     * to provide a function that returns $\log(\pi(x))$ instead
+     * of $\pi(x)$.
+     *
+     * The result of calling this class's sample() function is a sequence of
+     * samples $x_k$ approximating $\pi(x)$ and that are passed to all
+     * Consumer objects connected to the corresponding signal (by calling
+     * Producer::connect_to_signal() or using Consumer::connect_to_producer())
+     * one at a time. The AuxiliaryData object associated with a sample
+     * $x_k$ stores an entry with name "relative likelihood" of type
+     * `double` that stores $\pi(x_k)$.
+     */
     template <typename OutputType>
     class MetropolisHastings : public Producer<OutputType>
     {
       public:
-        void sample (const OutputType &starting_point,
-                     const std::function<double (const OutputType &)> &log_likelihood,
-                     const std::function<OutputType (const OutputType &)> &perturb,
-                     const unsigned int n_samples);
+        /**
+         * The principal function of this class. Starting from the given
+         * initial sample $x_0$, it produces a sequence of samples $x_k$
+         * that are passed through the signal of the base class to
+         * Consumer objects.
+         *
+         * @param[in] starting_point The initial sample $x_0$.
+         * @param[in] log_likelihood A function object that, when called
+         *   with a sample $x$, returns $\log(\pi(x))$, i.e., the natural
+         *   logarithm of the likelihood function evaluated at the sample.
+         * @param[in] perturb A function object that, when given a sample
+         *   $x$, returns a different sample $\tilde x$ that is perturbed
+         *   in some way. If samples are from some continuous space, say
+         *   ${\mathbb R}^n$, then the perturbation function is often
+         *   implemented by choosing $\tilde x$ from a neighborhood
+         *   of $x$.
+         * @param[in] n_samples The number of (new) samples to be produced
+         *   by this function. This is also the number of times the
+         *   signal is called that notifies Consumer objects that a new
+         *   sample is available.
+         */
+        void
+        sample (const OutputType &starting_point,
+                const std::function<double (const OutputType &)> &log_likelihood,
+                const std::function<OutputType (const OutputType &)> &perturb,
+                const unsigned int n_samples);
     };
 
 
@@ -50,14 +99,24 @@ namespace SampleFlow
       OutputType current_sample         = starting_point;
       double     current_log_likelihood = log_likelihood (current_sample);
 
-      this->issue_sample(current_sample,
-      {{"relative likelihood", boost::any(current_log_likelihood)}});
-
+      // Loop over the desired number of samples
       for (unsigned int i=0; i<n_samples; ++i)
         {
+          // Obtain a new sample by perturbation and evaluate the
+          // log likelihood for it
           const OutputType trial_sample         = perturb (current_sample);
           const double     trial_log_likelihood = log_likelihood (trial_sample);
 
+          // Then see if we want to accept the sample. This happens if either
+          // the new sample has a higher likelihood (which happens if and
+          // only if the log likelihood of the new sample is larger than the
+          // log likelihood of the old sample), or if the ratio of likelihoods
+          // is larger than a randomly drawn number between zero and one. The
+          // ratio of likelihoods equals the exp of the difference of
+          // log likelihoods.
+          //
+          // If the sample is not accepted, then we simply stick with (i.e.,
+          // repeat) the previous sample.
           if ((trial_log_likelihood > current_log_likelihood)
               ||
               (std::exp(trial_log_likelihood - current_log_likelihood) >= uniform_distribution(rng)))
@@ -66,6 +125,7 @@ namespace SampleFlow
               current_log_likelihood = trial_log_likelihood;
             }
 
+          // Output the new sample (which may be equal to the old sample).
           this->issue_sample (current_sample,
           {{"relative likelihood", boost::any(current_log_likelihood)}});
         }
