5eded0c347d041cc06c71009fe0ac75b955c5f5e
Wolfgang Bangerth <bangerth@colostate.edu>
2020-07-07 17:31:57 -0600

    Add tests.

---
+++ tests/auto_covariance_matrix_00_vector_02.cc
@@ -0,0 +1,66 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2019 by the SampleFlow authors.
+//
+// This file is part of the SampleFlow library.
+//
+// The SampleFlow library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of SampleFlow.
+//
+// ---------------------------------------------------------------------
+
+
+// Check the AutoCovarianceMatrix consumer.
+//
+// This test is the same as the auto_covariance_matrix_01 test, but
+// instead of scalar-valued samples, it uses vectors of size 2 for
+// which the first component is simply always zero and the second
+// component as in the original test
+
+#include <iostream>
+#include <valarray>
+
+#include <sampleflow/producers/range.h>
+#include <sampleflow/consumers/auto_covariance_matrix.h>
+#include <sampleflow/consumers/covariance_matrix.h>
+#include <sampleflow/consumers/stream_output.h>
+
+
+int main ()
+{
+  using SampleType = std::valarray<double>;
+
+  SampleFlow::Producers::Range<SampleType> range_producer;
+
+  const unsigned int max_lag = 10;
+  SampleFlow::Consumers::AutoCovarianceMatrix<SampleType> autocovariance(max_lag);
+  autocovariance.connect_to_producer (range_producer);
+
+  SampleFlow::Consumers::CovarianceMatrix<SampleType> cov;
+  cov.connect_to_producer (range_producer);
+
+  SampleFlow::Consumers::StreamOutput<SampleType> stream_output(std::cout);
+  stream_output.connect_to_producer(range_producer);
+
+  std::vector<SampleType> samples(20, std::valarray<double>(2));
+  for (unsigned int i=0; i<samples.size(); ++i)
+    samples[i][1] = (i % 2 == 0 ? -1. : 1.);
+
+  range_producer.sample (samples);
+
+  std::cout.precision(16);
+  std::cout << "Covariance matrix="
+            << cov.get()(0,0) << ' '
+            << cov.get()(0,1) << ' '
+            << cov.get()(1,0) << ' '
+            << cov.get()(1,1)
+            << std::endl;
+
+  std::cout << "Auto-covariances:" << std::endl;
+  for (const auto v : autocovariance.get())
+    std::cout << v << std::endl;
+}
---
+++ tests/auto_covariance_matrix_03.cc
@@ -0,0 +1,70 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2019 by the SampleFlow authors.
+//
+// This file is part of the SampleFlow library.
+//
+// The SampleFlow library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of SampleFlow.
+//
+// ---------------------------------------------------------------------
+
+
+// Check the AutoCovarianceMatrix consumer. Do so with a sequence of
+// samples that is truly uncorrelated: We just pick numbers in the range
+// [0,1] by random. For a sufficiently long such sequence, all
+// autocorrelations (with lag>0) should be close to zero.
+
+
+#include <iostream>
+#include <valarray>
+#include <random>
+
+#include <sampleflow/producers/range.h>
+#include <sampleflow/consumers/auto_covariance_matrix.h>
+
+
+int main ()
+{
+  using SampleType = std::valarray<double>;
+
+  // Run the following workflow 3 times with increasing numbers of
+  // samples
+  for (auto N :
+       {
+         100, 1000, 10000
+       })
+    {
+      SampleFlow::Producers::Range<SampleType> range_producer;
+
+      const unsigned int max_lag = 10;
+      SampleFlow::Consumers::AutoCovarianceMatrix<SampleType> autocovariance(max_lag);
+      autocovariance.connect_to_producer (range_producer);
+
+      std::vector<SampleType> samples(N, std::valarray<double>(1));
+      std::mt19937 rng;
+      std::uniform_real_distribution<> uniform_distribution(0,1);
+      for (unsigned int i=0; i<samples.size(); ++i)
+        samples[i][0] = uniform_distribution(rng);
+
+      range_producer.sample (samples);
+
+      std::cout << "Number of samples: " << N << std::endl;
+      for (const auto v : autocovariance.get())
+        std::cout << "  " << v << std::endl;
+
+      // Now also compute the average of these numbers and output
+      // that. Each of the samples are uncorrelated, so there is no
+      // reason why the AC(1) should be any different from AC(10), and
+      // we can just take the average. When computing the average,
+      // skip AC(0) since that just measures the variance of samples
+      double s = -autocovariance.get()[0];
+      for (const auto v : autocovariance.get())
+        s += v;
+      std::cout << "  average: " << s/max_lag << std::endl;
+    }
+}
