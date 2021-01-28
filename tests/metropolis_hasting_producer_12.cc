bb0220f242d32338f3869193f2e6dcf1f2c10389
dawsoneliasen <dawsoneliasen@gmail.com>
2021-01-28 14:34:06 -0700

    fix perturb, rotate cov

--- tests/metropolis_hasting_producer_12.cc
+++ tests/metropolis_hasting_producer_12.cc
@@ -15,8 +15,7 @@
 
 
 // Test the Metropolis-Hastings producer with a diagonally elongated
-// distribution; here, we use a MVN with mean [0, 0] and covariance
-// [[10, 0], [0, 1]].
+// distribution.
 
 #include <iostream>
 #include <sampleflow/producers/metropolis_hastings.h>
@@ -36,16 +35,39 @@ double log_likelihood (const SampleType &x)
 {
   double mu[2] = {0, 0};
   // double cov[2][2] = {{10, 0}, {0, 1}};
-  double cov_inv[2][2] = {{1/10, 0},{0, 1}};
-  double dev[2] = {x[0]- mu[0], x[1] - mu[1]};
-  double dev_covinv[2] = {dev[0] * cov_inv[0][0] + dev[1] * cov_inv[0][1],
-                          dev[0] * cov_inv[1][0] + dev[1] * cov_inv[1][1]};
+  double cov_inv[2][2] = {{1.0/10.0, 0},{0, 1}};
+
+  double phi = 3.1415 / 6;
+  double rotation_matrix[2][2] = {{cos(phi), sin(phi)},
+    {-sin(phi), cos(phi)}
+  };
+  double rotation_matrix_transpose[2][2] = {{cos(phi), -sin(phi)},
+    {sin(phi), cos(phi)}
+  };
+  double RC[2][2] = {{
+      rotation_matrix[0][0] *cov_inv[0][0] + rotation_matrix[0][1] *cov_inv[1][0],
+      rotation_matrix[0][0] *cov_inv[0][1] + rotation_matrix[0][1] *cov_inv[1][1]
+    },
+    {
+      rotation_matrix[1][0] *cov_inv[0][0] + rotation_matrix[1][1] *cov_inv[1][0],
+      rotation_matrix[1][0] *cov_inv[0][1] + rotation_matrix[1][1] *cov_inv[1][1]
+    }
+  };
+  double rotated_cov_inv[2][2] = {{
+      RC[0][0] *rotation_matrix_transpose[0][0] + RC[0][1] *rotation_matrix_transpose[1][0],
+      RC[0][0] *rotation_matrix_transpose[0][1] + RC[0][1] *rotation_matrix_transpose[1][1]
+    },
+    {
+      RC[1][0] *rotation_matrix_transpose[0][0] + RC[1][1] *rotation_matrix_transpose[1][0],
+      RC[1][0] *rotation_matrix_transpose[0][1] + RC[1][1] *rotation_matrix_transpose[1][1]
+    }
+  };
+
+  double dev[2] = {x[0] - mu[0], x[1] - mu[1]};
+  double dev_covinv[2] = {dev[0] *rotated_cov_inv[0][0] + dev[1] *rotated_cov_inv[0][1],
+                          dev[0] *rotated_cov_inv[1][0] + dev[1] *rotated_cov_inv[1][1]
+                         };
   double dev_covinv_dev = dev_covinv[0] * dev[0] + dev_covinv[1] * dev[1];
-  // return -0.5 * ((x[0]-mu[0])*cov_inv[0][0]*(x[0]-mu[0]) +
-  //                (x[0]-mu[0])*cov_inv[0][1]*(x[1]-mu[1]) +
-  //                (x[1]-mu[1])*cov_inv[1][0]*(x[0]-mu[0]) +
-  //                (x[1]-mu[1])*cov_inv[1][1]*(x[1]-mu[1]));
-  std::cout << x[0] << " " << -0.5 * dev_covinv_dev << std::endl;
   return -0.5 * dev_covinv_dev;
 }
 
@@ -56,7 +78,7 @@ std::pair<SampleType,double> perturb (const SampleType &x)
   std::normal_distribution<double> distribution(0, 1);
   SampleType y = x;
   for (auto &el : y)
-    y += distribution(rng);
+    el += distribution(rng);
   return {y, 1.0};
 }
 
@@ -68,8 +90,6 @@ int main ()
   SampleFlow::Consumers::MeanValue<SampleType> mean_value;
   cov_matrix.connect_to_producer(mh_sampler);
   mean_value.connect_to_producer(mh_sampler);
-  // SampleFlow::Consumers::StreamOutput<SampleType> output(std::cout);
-  // output.connect_to_producer(mh_sampler);
   // Sample, starting at an asymmetric point, and creating 100,000 samples
   mh_sampler.sample({5, -1}, &log_likelihood, &perturb, 100000);
   std::cout << "Mean value = " << mean_value.get()[0] << std::endl;
